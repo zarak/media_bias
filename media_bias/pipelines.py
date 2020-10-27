f83d01b2113692d10a957c5dee62bf366dcf8c7c
Zarak <7254237+zarak@users.noreply.github.com>
2020-10-27 22:59:31 +0500

    Add example mongo pipeline

--- media_bias/pipelines.py
+++ media_bias/pipelines.py
@@ -6,8 +6,39 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import pymongo
+import logging
 
 
 class MediaBiasPipeline:
     def process_item(self, item, spider):
+        return item
+
+class MongoPipeline(object):
+    def __init__(self, mongo_uri, mongo_db):
+        self.mongo_uri = mongo_uri
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            mongo_uri=crawler.settings.get('MONGO_URI'),
+            mongo_db=crawler.settings.get('MONGO_DATABASE', 'items')
+        )
+
+    def open_spider(self, spider):
+        # initializing spider
+        # opening db connection
+        self.client = pymongo.MongoClient(self.mongo_uri)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        # clean up when spider is closed
+        self.client.close()
+
+    def process_item(self, item, spider):
+        # how to handle each post
+        item_date = item['publish_date']
+        self.db[item_date].insert(dict(item))
+        logging.debug("Post added to MongoDB")
         return item
