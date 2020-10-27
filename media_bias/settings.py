870d76250dfdf8a1eb99826da85c75e3d2287872
Zarak <7254237+zarak@users.noreply.github.com>
2020-10-27 23:00:02 +0500

    Testing: add user agent info

--- media_bias/settings.py
+++ media_bias/settings.py
@@ -14,10 +14,11 @@ NEWSPIDER_MODULE = 'media_bias.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'media_bias (+http://www.yourdomain.com)'
+# USER_AGENT = 'media_bias (+https://github.com/zarak/media_bias.com)'
+USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64; rv:7.0.1) Gecko/20100101 Firefox/7.7'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -25,7 +26,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
