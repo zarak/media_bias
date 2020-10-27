a7c9508d1ca84f65ceae6291fc7a5007a406a82c
Zarak <7254237+zarak@users.noreply.github.com>
2020-10-27 23:00:57 +0500

    Fix formatting

--- media_bias/spiders/article.py
+++ media_bias/spiders/article.py
@@ -7,25 +7,21 @@ from newspaper import Article
 
 class ArticleSpider(scrapy.Spider):
     name = 'article'
-    start_urls = [
-        "http://www.usatoday.com/story/news/nation/2013/07/15/stateline-zimmerman-stand-your-ground/2517507/"	
-        "http://www.huffingtonpost.com/2013/05/21/senate-immigration-bill_n_3315271.html"	
-        "http://www.washingtonpost.com/opinions/dont-let-doma-fool-you--the-supreme-court-is-restricting-your-rights/2013/06/28/cd0afa1c-de85-11e2-b94a-452948b95ca8_story.html"	
-        "http://www.foxnews.com/politics/2013/04/03/obamacare-in-trouble-exchange-provision-delayed-as-lawmakers-push-to-repeal/"	
-        "http://www.breitbart.com/Big-Government/2013/10/09/Exclusive-Immigration-agents-rip-Bob-Goodlatte-other-Republicans-Democrats-pushing-for-amnesty-in-House"	
-        "http://www.cnn.com/2013/06/28/politics/obama-contraceptives/index.html"	
-        "http://www.foxnews.com/politics/2013/10/14/amid-cuts-to-federal-courts-judge-suggests-congress-go-to-hell/"	
-        "http://www.bbc.co.uk/news/technology-22213379"	
-        "http://www.nbcnews.com/technology/microsoft-let-nsa-bypass-encryption-mail-chats-cloud-storage-says-6C10607490"	
-        "http://radio.foxnews.com/toddstarnes/top-stories/military-blocks-access-to-southern-baptist-website.html"	
-        "http://www.bbc.co.uk/vietnamese/world/2013/08/130812_snowden_father_visit.shtml"	
-        "http://www.usatoday.com/story/nation/2013/08/07/jill-kelley-general-david-petraeus-general-john-allen/2625981"
-        "http://www.dailykos.com/story/2013/12/02/1259558/-Young-white-dancer-with-her-two-black-instructors-handcuffed-by-Houston-Police-Racism"	
-        "http://online.wsj.com/article/SB10001424127887323482504578227152762437278.html"
-        "http://www.chicagotribune.com/news/opinion/editorials/ct-illinois-pension-reform-edit-1118-20131118,0,5239008.story"
-    ]
+    start_urls = [ "http://www.foxnews.com/politics/2013/04/03/obamacare-in-trouble-exchange-provision-delayed-as-lawmakers-push-to-repeal/"	
+                 , "http://www.breitbart.com/Big-Government/2013/10/09/Exclusive-Immigration-agents-rip-Bob-Goodlatte-other-Republicans-Democrats-pushing-for-amnesty-in-House"	
+                 , "http://www.cnn.com/2013/06/28/politics/obama-contraceptives/index.html"	
+                 , "http://www.foxnews.com/politics/2013/10/14/amid-cuts-to-federal-courts-judge-suggests-congress-go-to-hell/"	
+                 , "http://www.bbc.co.uk/news/technology-22213379"	
+                 , "http://www.nbcnews.com/technology/microsoft-let-nsa-bypass-encryption-mail-chats-cloud-storage-says-6C10607490"	
+                 , "http://radio.foxnews.com/toddstarnes/top-stories/military-blocks-access-to-southern-baptist-website.html"	
+                 , "http://www.bbc.co.uk/vietnamese/world/2013/08/130812_snowden_father_visit.shtml"	
+                 , "http://www.usatoday.com/story/nation/2013/08/07/jill-kelley-general-david-petraeus-general-john-allen/2625981"
+                 , "http://www.dailykos.com/story/2013/12/02/1259558/-Young-white-dancer-with-her-two-black-instructors-handcuffed-by-Houston-Police-Racism"	
+                 , "http://online.wsj.com/article/SB10001424127887323482504578227152762437278.html"
+                 , "http://www.chicagotribune.com/news/opinion/editorials/ct-illinois-pension-reform-edit-1118-20131118,0,5239008.story" 
+                 ]
 
-    def parse_article(self, response):
+    def parse(self, response):
         article = Article(response.url)
         article.download()
         article.parse()
