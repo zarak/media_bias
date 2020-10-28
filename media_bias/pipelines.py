7374e29caac2fda2cb5dd620720abfbb985145af
Zarak <7254237+zarak@users.noreply.github.com>
2020-10-28 10:08:17 +0500

    Add deepblue pipeline

--- media_bias/pipelines.py
+++ media_bias/pipelines.py
@@ -6,7 +6,6 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-import pymongo
 import logging
 
 
@@ -14,31 +13,31 @@ class MediaBiasPipeline:
     def process_item(self, item, spider):
         return item
 
-class MongoPipeline(object):
-    def __init__(self, mongo_uri, mongo_db):
-        self.mongo_uri = mongo_uri
-        self.mongo_db = mongo_db
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        return cls(
-            mongo_uri=crawler.settings.get('MONGO_URI'),
-            mongo_db=crawler.settings.get('MONGO_DATABASE', 'items')
-        )
-
-    def open_spider(self, spider):
-        # initializing spider
-        # opening db connection
-        self.client = pymongo.MongoClient(self.mongo_uri)
-        self.db = self.client[self.mongo_db]
-
-    def close_spider(self, spider):
-        # clean up when spider is closed
-        self.client.close()
+class DeepBluePipeline(object):
+    def __init__(self):
+        engine = db_connect()
+        create_table(engine)
+        self.Session = sessionmaker(bind=engine)
 
     def process_item(self, item, spider):
-        # how to handle each post
-        item_date = item['publish_date']
-        self.db[item_date].insert(dict(item))
-        logging.debug("Post added to MongoDB")
+        session = self.Session()
+        deepblue = DeepBlue()
+        deepblue.text = item["text"]
+
+        # check whether the url exists
+        exist_url = session.query(DeepBlue).filter_by(url=item["url"]).first()
+        if exist_url is not None:  # the current url exists
+            deepblue.url = item["url"]
+
+        try:
+            session.add(deepblue)
+            session.commit()
+
+        except:
+            session.rollback()
+            raise
+
+        finally:
+            session.close()
+
         return item
